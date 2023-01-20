d5862d8fb8f01ee41b873930c58b5524fe6ed41e
Aleksey Ilyin <alksily@outlook.com>
2023-01-20 17:41:14 +0200

    add TranslateLetters

--- LangUkrainian/LangUkrainianPlugin.php
+++ LangUkrainian/LangUkrainianPlugin.php
@@ -13,12 +13,27 @@ class LangUkrainianPlugin extends AbstractPlugin
     const TITLE = 'Українська мова';
     const AUTHOR = 'Sofiia Skladanivska';
     const AUTHOR_SITE = 'https://getwebspace.org';
-    const VERSION = '1.1';
+    const VERSION = '1.1.1';
 
     public function __construct(ContainerInterface $container)
     {
         parent::__construct($container);
 
         $this->addLocaleFromFile('uk-UA', __DIR__ . '/Locale/_.php');
+
+        $this->addLocaleTranslateLetters('uk-UA',
+            [
+                'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+                'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ю', 'Я', 'Ь', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е',
+                'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                'ч', 'ш', 'щ', 'ю', 'я', 'ь',
+            ],
+            [
+                'A', 'B', 'V', 'H', 'G', 'D', 'E', 'Ye', 'Zh', 'Z', 'Y', 'I', 'Yi', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                'R', 'S', 'T', 'U', 'F', 'X', 'C', 'Ch', 'Sh', 'Shh', 'Yu', 'Ya', '', 'a', 'b', 'v', 'h', 'g', 'd', 'e',
+                'ye', 'zh', 'z', 'y', 'i', 'yi', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'x', 'c',
+                'ch', 'sh', 'shh', 'yu', 'ya', '',
+            ]
+        );
     }
 }
